8494fa4e4b6f5c6aba1906e3d37d42f94ede4481
ryan.nowacoski <rnowaco@us.ibm.com>
2017-12-03 23:50:13 -0500

    Day3 Part1 Solution

--- Day3/SolutionPart1.java
+++ Day3/SolutionPart1.java
@@ -1,10 +1,44 @@
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SolutionPart1{
     private static final int INPUT = 312051;
 
+    private class Cell {
+        private Point p;
+
+        public Point getPoint() {
+            return p;
+        }
+
+        public void setPoint(Point p) {
+            this.p = p;
+        }
+
+        public Integer getEnd() {
+            return end;
+        }
+
+        public void setEnd(Integer end) {
+            this.end = end;
+        }
+
+        private Integer end;
+
+        public Cell(Point p, Integer end){
+            setEnd(end);
+            setPoint(p);
+        }
+
+
+    }
+
     public static void main(String[] args) {
+        new SolutionPart1().solve();
+    }
+
+    private void solve() {
         // Initial differences between squares from center in each direction
         int r = 1;
         int u = 3;
@@ -14,13 +48,13 @@ public class SolutionPart1{
         // After initial difference, the differences increase by 8
         int diff = 8;
 
-        int center = 1; 
+        int center = 1;
         int cr = center;
         int cl = center;
         int cu = center;
         int cd = center;
 
-        List<Integer> ends = new ArrayList<>();
+        List<Cell> ends = new ArrayList<>();
 
         //Counter is number of spaces in given direction
         //cr counts the number at that space
@@ -30,7 +64,7 @@ public class SolutionPart1{
             r += diff;
             counter++;
         } while(cr < INPUT);
-        ends.add(cr);
+        ends.add(new Cell(new Point(counter,0), cr));
         System.out.println(counter);
 
         counter = 0;
@@ -39,7 +73,7 @@ public class SolutionPart1{
             l += diff;
             counter++;
         } while(cl < INPUT);
-        ends.add(cl);
+        ends.add(new Cell(new Point(counter,0), cl));
         System.out.println(counter);
 
         counter = 0;
@@ -48,7 +82,7 @@ public class SolutionPart1{
             u += diff;
             counter++;
         } while(cu < INPUT);
-        ends.add(cu);
+        ends.add(new Cell(new Point(0,counter), cu));
         System.out.println(counter);
 
         counter = 0;
@@ -57,18 +91,26 @@ public class SolutionPart1{
             d += diff;
             counter++;
         } while(cd < INPUT);
-        ends.add(cd);
+        ends.add(new Cell(new Point(0,counter), cd));
         System.out.println(counter);
 
-        int diffs = 0;
-        for(int i : ends){
-            int temp = Math.abs(i - INPUT);
+        int abs_diff = 0;
+        Point closest = new Point(0,0);
+        for(Cell c : ends){
+            Point p = c.getPoint();
+            int i = c.getEnd();
 
-            if(temp < diffs || diffs == 0){
-                diffs = temp;
+            int dd = i - INPUT;
+            int temp = Math.abs(dd);
+
+            if(temp < abs_diff || abs_diff == 0){
+                abs_diff = temp;
+                closest.x = p.x != 0 ? p.x : abs_diff;
+                closest.y = p.y != 0 ? p.y : abs_diff;
             }
         }
 
-        System.out.println(diffs);
+        System.out.println(Math.abs(closest.x) + Math.abs(closest.y));
     }
+
 }
