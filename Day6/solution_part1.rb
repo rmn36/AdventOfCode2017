c4e91ccadb66f74ed3d97bb0819afd8a94bdb0dd
ryan.nowacoski <rnowaco@us.ibm.com>
2017-12-07 14:03:17 -0500

    Day6 Part1 and 2 solutions

---
+++ Day6/solution_part1.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+require 'set'
+
+set = Set.new []
+x = []
+f = File.read("input.txt").split("\t").map(&:to_i)
+set.add(f.join(','))
+
+old_size = set.size
+new_size = old_size
+counter = 0
+begin
+    val = f.max
+    indx = f.index(val)
+    f[indx] = 0
+
+    while val != 0
+        indx += 1
+        if indx >= f.length 
+            indx = indx - f.length
+        end
+        f[indx] += 1
+        val -= 1
+    end
+
+    set.add(f.join(','))
+    x.push(f.join(','))
+    old_size = new_size
+    new_size = set.size
+    counter+= 1
+
+end while old_size != new_size
+
+puts 
+puts counter - x.index(f.join(',')) - 1
